9c223a3d404dcaa52ce763a8b58be0ca8ecb3217
Jose Martins <josemartins90@gmail.com>
2023-03-11 18:30:31 +0000

    add missing riscv tlb helpers

--- core/arch/riscv/include/kernel/tlb_helpers.h
+++ core/arch/riscv/include/kernel/tlb_helpers.h
@@ -10,6 +10,14 @@
 
 void tlbi_all(void);
 void tlbi_asid(unsigned long asid);
+void tlbi_mva_allasid(unsigned long addr);
+static inline void tlbi_mva_allasid_nosync(unsigned long va) {
+    tlbi_mva_allasid(va);
+}
+void tlbi_mva_asid(unsigned long va, uint32_t asid);
+static inline void tlbi_mva_asid_nosync(unsigned long va, uint32_t asid) {
+    tlbi_mva_asid(va, asid);
+}
 
 #endif /*!__ASSEMBLER__*/
 
